5db098dc22a6dd3a7bbcd73a4f723977eac17f0b
RudzaniMatidze <rdzmatidze@gmail.com>
2024-01-17 11:49:19 -0800

    errors fixed

--- mul.c
+++ mul.c
@@ -11,7 +11,7 @@ void f_mul(stack_t **head, unsigned int counter)
 	stack_t *h;
 	int len = 0, aux;
 
-	head = *head;
+	h = *head;
 	while (h)
 	{
 		h = h->next;
@@ -29,5 +29,5 @@ void f_mul(stack_t **head, unsigned int counter)
 	aux = h->next->n * h->n;
 	h->next->n = aux;
 	*head = h->next;
-	free(h)
+	free(h);
 }
