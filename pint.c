c85ba35da3b15ff7dd5cc898525ec73d7fe50f0d
RudzaniMatidze <rdzmatidze@gmail.com>
2024-01-17 08:28:31 -0800

    file fixed

--- pint.c
+++ pint.c
@@ -11,7 +11,7 @@ void f_pint(stack_t **head, unsigned int counter)
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		fclose(bus.content);
+		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
